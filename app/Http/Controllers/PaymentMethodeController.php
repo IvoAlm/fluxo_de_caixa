e2f058b77441749963b48dd99a72afb21b551f56
Ivo Almeida <ivodsalmeida@gmail.com>
2020-12-06 01:37:34 -0300

    create seeds, models and migration first part

---
+++ app/Http/Controllers/PaymentMethodeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\payment_methode;
+use Illuminate\Http\Request;
+
+class PaymentMethodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\payment_methode  $payment_methode
+     * @return \Illuminate\Http\Response
+     */
+    public function show(payment_methode $payment_methode)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\payment_methode  $payment_methode
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(payment_methode $payment_methode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\payment_methode  $payment_methode
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, payment_methode $payment_methode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\payment_methode  $payment_methode
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(payment_methode $payment_methode)
+    {
+        //
+    }
+}
---
+++ database/factories/PaymentMethodeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\payment_methode;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PaymentMethodeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = payment_methode::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
